1227f776b8a23f63703986e6421869c154d3021f
Vinson Chuong <vinsonchuong@gmail.com>
2016-03-28 20:34:59 -0700

    Add hot reloading to jspm project

--- jspm/jspm.config.js
+++ jspm/jspm.config.js
@@ -3,6 +3,7 @@ SystemJS.config({
   packages: {
     "jspm-app": {
       "main": "index.js",
+      "format": "esm",
       "meta": {
         "*.js": {
           "loader": "plugin-babel"
@@ -19,12 +20,29 @@ SystemJS.config({
     "github:*/*.json"
   ],
   map: {
+    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+    "net": "github:jspm/nodelibs-net@0.2.0-alpha",
     "plugin-babel": "npm:systemjs-plugin-babel@0.0.8",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "react": "npm:react@0.14.7",
-    "react-dom": "npm:react-dom@0.14.7"
+    "react-dom": "npm:react-dom@0.14.7",
+    "systemjs-hot-reloader": "github:capaj/systemjs-hot-reloader@0.5.6",
+    "tty": "github:jspm/nodelibs-tty@0.2.0-alpha",
+    "util": "github:jspm/nodelibs-util@0.2.0-alpha"
   },
   packages: {
+    "github:capaj/systemjs-hot-reloader@0.5.6": {
+      "map": {
+        "debug": "npm:debug@2.2.0",
+        "socket.io-client": "github:socketio/socket.io-client@1.4.5",
+        "weakee": "npm:weakee@1.0.0"
+      }
+    },
+    "npm:debug@2.2.0": {
+      "map": {
+        "ms": "npm:ms@0.7.1"
+      }
+    },
     "npm:react@0.14.7": {
       "map": {
         "fbjs": "npm:fbjs@0.6.1"
